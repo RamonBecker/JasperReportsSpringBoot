4bbf62bebc2364d5db47b8fc63136f68723f3d48
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2021-12-22 18:49:35 -0300

    Preparando o método para exportar o relatório em PDF

---
+++ demo-jasper-spring/src/main/java/com/mballem/curso/jasper/spring/service/JasperService.java
@@ -0,0 +1,48 @@
+
+package com.mballem.curso.jasper.spring.service;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+
+@Service
+public class JasperService {
+
+	private static final String JASPER_DIRETORIO = "classpath:jasper/";
+	private static final String JASPER_PREFIXO = "funcionarios-";
+	private static final String JASPER_SULFIXO = ".jasper";
+
+	@Autowired
+	private Connection connection;
+
+	private Map<String, Object> params = new HashMap<String, Object>();
+
+	public void addParams(String key, Object value) {
+		params.put(key, value);
+	}
+
+	public byte[] exportarPDF(String code) {
+		byte[] bytes = null;
+		String arq = JASPER_DIRETORIO.concat(JASPER_PREFIXO).concat(code).concat(JASPER_SULFIXO);
+		try {
+			File file = ResourceUtils.getFile(arq);
+			JasperPrint print = JasperFillManager.fillReport(file.getAbsolutePath(), params, connection);
+			bytes = JasperExportManager.exportReportToPdf(print);
+		} catch (FileNotFoundException | JRException e) {
+			e.printStackTrace();
+		}
+
+		return bytes;
+	}
+}
